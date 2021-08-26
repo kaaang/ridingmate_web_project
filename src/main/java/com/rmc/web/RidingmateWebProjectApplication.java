9bc5cf8fe095e535a1967b1548f141c2d4f3100b
kaaang <kaaang@kakao.com>
2021-08-26 14:36:59 +0900

    start

---
+++ src/main/java/com/rmc/web/RidingmateWebProjectApplication.java
@@ -0,0 +1,13 @@
+package com.rmc.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RidingmateWebProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RidingmateWebProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rmc/web/RidingmateWebProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rmc.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RidingmateWebProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
